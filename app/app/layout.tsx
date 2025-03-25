ab419d04c291fa69a908c7de343dc40f21961ac0
eduuusama <esamayoam1@gmail.com>
2025-03-24 21:00:24 -0500

    Update font styles in globals.css and layout.tsx; replace Geist fonts with Source Sans 3 for improved typography

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const sourceSansPro = Source_Sans_3({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-source-sans",
 });
 
 export const metadata: Metadata = {
@@ -24,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${sourceSansPro.variable} antialiased`}>
         {children}
       </body>
     </html>
